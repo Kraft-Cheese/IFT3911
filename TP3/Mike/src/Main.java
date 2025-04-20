f6d4e48b5c05a79840a02a0c81f0a0da16b679b8
atcoughlan <ameliecoughlan@hotmail.com>
2025-04-20 10:08:14 -0400

    added opencsv + apache common lang + read and write csv

--- TP3/Mike/src/Main.java
+++ TP3/Mike/src/Main.java
@@ -1,5 +1,28 @@
+import Database.Database;
+
+import java.util.ArrayList;
+
 public class Main {
-    public static void main(String[] args) {
-        System.out.println("Hello world!");
+    public static void main(String[] args)
+    {
+        /*
+        ArrayList<String[]> csvLines = DATABASE.readCSV();
+        for (String[] line : csvLines)
+        {
+            for (String element : line)
+            {
+                System.out.print(element + " - ");
+            }
+            System.out.print("\n");
+        }
+        String[] a = {"allo", "bye"};
+        String[] b = {"woop", "sloop"};
+        ArrayList<String[]> test = new ArrayList<>();
+        test.add(a);
+        test.add(b);
+        DATABASE.writeCSV(test);
+        */
     }
+
+    // private static Database DATABASE = new Database();
 }
